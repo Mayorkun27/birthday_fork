fd448e5b3be9f5bfcb2f3eb756f70ffb9b8b494e
seniorcub <farinderif@gmail.com>
2024-11-17 13:52:17 +0100

    Initial Commit

---
+++ page2/js/index.js
@@ -0,0 +1,37 @@
+(function() {
+  function $(id) {
+    return document.getElementById(id);
+  }
+
+  var card = $('card'),
+      openB = $('open'),
+      closeB = $('close'),
+      closeC = $('closee')
+      timer = null;
+  console.log('wat', card);
+  openB.addEventListener('click', function () {
+    card.setAttribute('class', 'open-half');
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', 'open-fully');
+      timer = null;
+    }, 1000);
+  });
+
+  closeB.addEventListener('click', function () {
+    card.setAttribute('class', 'close-half');
+    if (timer) clearTimerout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', '');
+      timer = null;
+    }, 1000);
+  });
+
+  closeC.addEventListener('click', function () {
+    if (timer) clearTimerout(timer);
+    timer = setTimeout(function () {
+     window.location.href = '/page3/'; // Replace with your target URL
+    }, 1000);
+  });
+
+}());
